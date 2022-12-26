5da5559ae2250cc4507b2b3528cd3344995126cc
Gabor Orban <orbangabeszb@gmail.com>
2022-12-26 19:21:40 +0100

    ExportFileReader minor refactoring

--- src/main/java/org/example/util/ExportFileReader.java
+++ src/main/java/org/example/util/ExportFileReader.java
@@ -13,16 +13,19 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
-import java.util.regex.Pattern;
 
 @UtilityClass
 public final class ExportFileReader {
     private static final int ASSET_NAME_INDEX = 1;
     private static final int UNITS_INDEX = 4;
-    private static final int SUCCESSFUL_INDEX = 6;
+    private static final int SUCCESS_INDEX = 6;
     private static final int AMOUNT_IN_USD_INDEX = 2;
     private static final String DELIMITER_SPACE = " ";
     private static final String SUCCESS_STATUS = "Success";
+    private static final List<Integer> USED_COLUMN_INDEX_LIST = Arrays.asList(ASSET_NAME_INDEX,
+                                                                              UNITS_INDEX,
+                                                                              SUCCESS_INDEX,
+                                                                              AMOUNT_IN_USD_INDEX);
 
     public static Collection<Asset> readExportFile(String filePath) throws IOException {
         FileInputStream inputFile = new FileInputStream(filePath);
@@ -36,14 +39,15 @@ public final class ExportFileReader {
             }
             Asset asset = new Asset();
             for (Cell cell : row) {
-                if (isColumnUsed(cell.getColumnIndex())) {
+                int columnIndex = cell.getColumnIndex();
+                if (USED_COLUMN_INDEX_LIST.contains(columnIndex)) {
                     String cellValue = cell.getRichStringCellValue().getString();
-                    switch (cell.getColumnIndex()) {
+                    switch (columnIndex) {
                         case ASSET_NAME_INDEX -> asset.setAssetName(cellValue);
                         case UNITS_INDEX -> asset.setUnits(new BigDecimal(getPureValue(cellValue)));
-                        case SUCCESSFUL_INDEX -> asset.setSuccessful(SUCCESS_STATUS.equals(cellValue));
+                        case SUCCESS_INDEX -> asset.setSuccessful(SUCCESS_STATUS.equals(cellValue));
                         case AMOUNT_IN_USD_INDEX -> asset.setAmountInUsd(new BigDecimal(getPureValue(cellValue)));
-                        default -> System.out.printf("Column %s is not implemented!%n", cell.getColumnIndex());
+                        default -> System.out.printf("Column %s is not implemented!%n", columnIndex);
                     }
                 }
             }
@@ -54,16 +58,7 @@ public final class ExportFileReader {
         return assetSet;
     }
 
-    private static boolean isColumnUsed(int columnIndex) {
-        return columnIndex == ASSET_NAME_INDEX ||
-                columnIndex == UNITS_INDEX ||
-                columnIndex == SUCCESSFUL_INDEX ||
-                columnIndex == AMOUNT_IN_USD_INDEX;
-    }
-
     private static String getPureValue(String cellValue) {
-        Pattern pattern = Pattern.compile(DELIMITER_SPACE);
-        return pattern.split(cellValue)[0];
+        return cellValue.split(DELIMITER_SPACE)[0];
     }
-
 }
